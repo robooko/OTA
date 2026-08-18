b99cd1aac8a88f173a038c3c711452ac56fd9830
Robert Okoroafor <robooko@hotmail.com>
2026-08-18 08:23:02 +0100

    Allow API key auth on all pro shop routes

--- src/routes/proshop.js
+++ src/routes/proshop.js
@@ -1,20 +1,20 @@
 const router = require('express').Router();
 const ctrl = require('../controllers/proshop');
-const { authenticate } = require('../middleware/auth');
+const { authenticateOrApiKey } = require('../middleware/auth');
 
 // Shops
-router.get('/shops', authenticate, ctrl.listShops);
-router.post('/shops', authenticate, ctrl.createShop);
-router.put('/shops/:id', authenticate, ctrl.updateShop);
+router.get('/shops', authenticateOrApiKey, ctrl.listShops);
+router.post('/shops', authenticateOrApiKey, ctrl.createShop);
+router.put('/shops/:id', authenticateOrApiKey, ctrl.updateShop);
 
 // Catalogue
-router.get('/items', authenticate, ctrl.listItems);
-router.post('/items', authenticate, ctrl.createItem);
-router.put('/items/:id', authenticate, ctrl.updateItem);
+router.get('/items', authenticateOrApiKey, ctrl.listItems);
+router.post('/items', authenticateOrApiKey, ctrl.createItem);
+router.put('/items/:id', authenticateOrApiKey, ctrl.updateItem);
 
 // Booking items
-router.get('/booking/:booking_id', authenticate, ctrl.listBookingItems);
-router.post('/booking/:booking_id', authenticate, ctrl.addBookingItem);
-router.delete('/booking/:booking_id/:id', authenticate, ctrl.removeBookingItem);
+router.get('/booking/:booking_id', authenticateOrApiKey, ctrl.listBookingItems);
+router.post('/booking/:booking_id', authenticateOrApiKey, ctrl.addBookingItem);
+router.delete('/booking/:booking_id/:id', authenticateOrApiKey, ctrl.removeBookingItem);
 
 module.exports = router;
